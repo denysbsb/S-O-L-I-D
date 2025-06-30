47b9b651a46589853b021759e271a121744e6fb9
denysbsb <denys.design@gmail.com>
2025-06-30 14:56:36 -0300

    dependency inversion principle

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # S-O-L-I-D
+
+D.
+
+Dependency inversion
+
+High level modules should not depend low level modules
+
+Módulos de alto nível não devem depender de módulos de baixo nível-Dependa de abstrações, não de implementações
---
+++ dependency-inversion.kt
@@ -0,0 +1,50 @@
+//D.
+//Dependency inversion
+//Principio de inversão de dependencia
+//modulos alto nivel nao podem depender de baixo nivel - Dependa de abstrações, não de implementações
+
+/* Exemplo errado */
+class Email{
+    fun notificar(destinatario:String, txt: String){
+        //Enviar email
+    }
+}
+
+class Notificador{
+    val service = Email()
+
+    fun enviar(destinatario:String, txt: String){
+        service.notificar(txt)
+    }
+}
+
+/* Exemplo correto */
+implement ServicoNotificacao {
+    fun notificar(destinatario:String, txt: String){}
+}
+
+class EmailServiceNotification: ServicoNotificacao {
+    override notificar(destinatario:String, txt: String){
+        //Enviar email
+    }
+}
+
+class SmsServiceNotification: ServicoNotificacao {
+    override notificar(destinatario:String, txt: String){
+        //Enviar sms
+    }
+}
+
+class Notificador(service: ServicoNotificacao) {
+    fun notificar(destinatario:String, txt: String){
+        service.notificar(destinatario:String, txt: String)
+    }
+}
+
+class main(){
+    val notificadorEmail = Notificador(EmailServiceNotification())
+    notificadorEmail.notificar("joao@email.com", "Sua fatura venceu!")
+
+    val notificadorSms = Notificador(SmsServiceNotification())
+    notificadorSms.notificadorSms("6198111112","Codigo de verificacao")
+}
